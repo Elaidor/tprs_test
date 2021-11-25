730390d56113e87572b715e37b53d1b01630d46d
Liubov <luba_siagaeva@mail.ru>
2021-11-26 02:50:47 +0300

    dropdown

--- src/components/page-blocks/js/_project-block-dropdown.js
+++ src/components/page-blocks/js/_project-block-dropdown.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
     $(this).children('.project-dropdown__information').slideToggle('normal');
     return false;
   });
+
+  $('.project-block_click').click(function () {
+    $('.project-dropdown_item').children('.dropdown-icon').toggleClass('transform');
+  });
 });
