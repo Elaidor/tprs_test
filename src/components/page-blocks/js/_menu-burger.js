65b242d98fad463c26cacd70173a5ea0d7d5e94f
Liubov <luba_siagaeva@mail.ru>
2021-11-26 00:15:18 +0300

    prettify configs

--- src/components/page-blocks/js/_menu-burger.js
+++ src/components/page-blocks/js/_menu-burger.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-    $('.menu-burger').click(function() {
-        $('.menu-burger').toggleClass('open-menu');
-        $('.main-nav').toggleClass('open-menu');
-        $('body').toggleClass('fixed-page');
-    });
+$(document).ready(function () {
+  $('.menu-burger').click(function () {
+    $('.menu-burger').toggleClass('open-menu');
+    $('.main-nav').toggleClass('open-menu');
+    $('body').toggleClass('fixed-page');
+  });
 });
--- tasks/watch.js
+++ tasks/watch.js
@@ -1,15 +1,11 @@
-const {
-	watch,
-	parallel,
-	series
-} = require('gulp');
+const { watch, parallel, series } = require('gulp');
 
 module.exports = function watching() {
-	watch('src/**/*.html', parallel('html'));
-	watch('src/**/*.scss', parallel('style'));
-	watch('src/**/*.js', parallel('dev_js'));
-	watch('src/**/*.json', parallel('html'));
-	watch('src/img/**/*.+(png|jpg|jpeg|gif|svg|ico)', parallel('rastr'));
-	watch('build/img/**/*.+(png|jpg|jpeg)');
-	watch('src/fonts/**/*.ttf', series('ttf', 'fonts'));
-}
+  watch('src/**/*.html', parallel('html'));
+  watch('src/**/*.scss', parallel('style'));
+  watch('src/**/*.js', parallel('dev_js'));
+  watch('src/**/*.json', parallel('html'));
+  watch('src/img/**/*.+(png|jpg|jpeg|gif|svg|ico)', parallel('rastr'));
+  watch('build/img/**/*.+(png|jpg|jpeg)');
+  watch('src/fonts/**/*.ttf', series('ttf', 'fonts'));
+};
