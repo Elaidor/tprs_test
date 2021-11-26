af8b846acd92eb3e4e7aaf41017c503f92d54451
Liubov <luba_siagaeva@mail.ru>
2021-11-26 04:58:31 +0300

    prettier

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const requireDir = require('require-dir');
 const tasks = requireDir('./tasks');
 
 exports.style = tasks.style;
-exports.libs_style = tasks.libs_style;
 exports.build_js = tasks.build_js;
 exports.libs_js = tasks.libs_js;
 exports.dev_js = tasks.dev_js;
@@ -15,7 +14,6 @@ exports.bs_html = tasks.bs_html;
 exports.watch = tasks.watch;
 
 exports.default = gulp.parallel(
-  exports.libs_style,
   exports.style,
   exports.libs_js,
   exports.dev_js,
